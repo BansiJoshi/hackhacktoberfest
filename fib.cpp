33e1aaed4451f10f50557009edd620459778504a
BANSI JOSHI <bkjoshi14297@gmail.com>
2018-10-07 23:01:02 +0530

    Update fib.cpp

--- fib.cpp
+++ fib.cpp
@@ -6,7 +6,7 @@ void printFibonacci(int n){
          n3 = n1 + n2;    
          n1 = n2;    
          n2 = n3;    
- cout<<n3<<" ";    
+         cout<<n3<<" ";    
          printFibonacci(n-1);    
     }    
 }    
