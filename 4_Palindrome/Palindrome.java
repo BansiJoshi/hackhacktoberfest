4b8bb4c7018c3b5a078385eea39744662c8fd88b
Pratik Sanghani <pratiksanghani70@gmail.com>
2019-10-09 21:19:59 +0530

    Update Palindrome.java

--- 4_Palindrome/Palindrome.java
+++ 4_Palindrome/Palindrome.java
@@ -1,16 +1,16 @@
 class Palindrome{
- public static void main(String args[]){  
-  int a,sum=0,temp;    
-  int n=121;
-  temp=n;    
-  while(n>0){    
-   a=n%10;  //getting remainder  
-   sum=(sum*10)+a;    
-   n=n/10;    
-  }    
-  if(temp==sum)    
-   System.out.println("palindrome number ");    
-  else    
-   System.out.println("not palindrome");    
-}  
+    public static void main(String args[]){  
+        int a,sum=0,temp;    
+        int n=121;
+        temp=n;    
+        while(n>0){    
+            a=n%10; 
+            sum=(sum*10)+a;    
+            n=n/10;    
+        }    
+        if(temp==sum)    
+            System.out.println("palindrome number ");    
+        else    
+            System.out.println("not palindrome");    
+    }  
 }  
